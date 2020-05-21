efd79b513cb46c297798144105b4aedfbf4677cf
lizjack <65046262+lizjack@users.noreply.github.com>
2020-05-20 18:13:19 -0700

    Began Project
    Added global variables, showPage function, and began working on appendPageLinks function

--- js/script.js
+++ js/script.js
@@ -9,14 +9,15 @@ FSJS project 2 - List Filter and Pagination
 /*** 
    Add your global variables that store the DOM elements you will 
    need to reference and/or manipulate. 
-   
+
    But be mindful of which variables should be global and which 
    should be locally scoped to one of the two main functions you're 
    going to create. A good general rule of thumb is if the variable 
    will only be used inside of a function, then it can be locally 
    scoped to that function.
 ***/
-
+const studentList = document.querySelector('.student-list');
+const perPage = 10;
 
 
 
@@ -35,14 +36,44 @@ FSJS project 2 - List Filter and Pagination
        "invoke" the function 
 ***/
 
-
+function showPage(list, page) {
+   let startIndex = (page * perPage) - perPage;
+   let endIndex = page * perPage;
+      for (let i = 0; i < list.length; i ++) {
+         if (list[i] >= startIndex || list[i] <= endIndex) {
+            //that item should be shown on the page
+            list[i].style.display = '';
+         } else {
+            list[i].style.display = 'none';
+         }
+        // the page should show the item 
+      }
+};
 
 
 /*** 
    Create the `appendPageLinks function` to generate, append, and add 
    functionality to the pagination buttons.
 ***/
+function appendPageLinks(list) {
+   const pagesNeeded = list.length / 10;
+   const page = document.querySelector('.page');
+   const div = document.createElement('div');
+   const ul = document.createElement('ul');
+   div.className = 'pagination';
+   page.appendChild(div);
+   div.appendChild(ul);
+      for (let i = 0; i < pagesNeeded.length; i ++) {
+         pagesNeeded[i].textContent = 
+      }
 
+
+
+   //a nested UL elemet containing one LI element for every 10 names in the list
+   //you can divide list.length by max numver of items per page(10) to figure
+   //out how many pages are needed, and use a loop that iterates
+   // that many times to create the correct number of Li elements
+}
 
 
 
